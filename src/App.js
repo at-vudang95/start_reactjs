99944cbc3b21300d56867d3c01c4e2cd8f442fc9
at-vudang95 <ngocvupct1995@gmail.com>
2017-10-25 10:59:28 +0700

    List map

--- src/App.js
+++ src/App.js
@@ -3,19 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {items: []}
+  }
+  componentWillMount() {
+    fetch('/data.json').then(res => res.json())
+        .then( ({results: items}) => {
+              this.setState({items})
+        }
+        );
+
+  }
+  filter(e) {
+    this.setState({filter: e.target.value})
+  }
   render() {
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter( item => item.name.toLowerCase()
+          .includes(this.state.filter.toLowerCase()))
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div>
+         <input type="text" onChange={this.filter.bind(this)}/>
+         {items.map(item =>
+               <List key={item.name} name={item.name}></List>
+         )}
+       </div>
       </div>
     );
   }
 }
+const List = (props) =>
+  <h4>{props.name}</h4>;
 
 export default App;
